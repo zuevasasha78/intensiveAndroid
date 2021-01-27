c58407042ada86bbfc8716e9c95b307635f01279
zuevasasha78 <zuevasasha78@gmail.com>
2021-01-27 20:32:21 +0300

    заигнорила keystore

--- app/src/main/java/ru/mikhailskiy/intensiv/MainActivity.kt
+++ app/src/main/java/ru/mikhailskiy/intensiv/MainActivity.kt
@@ -28,5 +28,12 @@ class MainActivity : AppCompatActivity() {
         val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav_view)
         bottomNav?.setupWithNavController(navController)
     }
+
+    companion object {
+        private val TAG = MainActivity::class.java.simpleName
+        private val API_KEY = BuildConfig.THE_MOVIE_DATABASE_API
+    }
 }
+
+
 
